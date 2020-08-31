2d8c5eb6480ee031d1203022e48622278b399a2c
jbtamaresgit <jbtamaresgit@outlook.com>
2020-08-31 16:10:56 +0800

    update chat controller based on quick start fb msgr

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -5,26 +5,34 @@ let postWebHook = (req, res) => {
 
     // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
-  
-      // Iterate over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-        // Get the webhook event. entry.messaging is an array, but 
-        // will only ever contain one event, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
 
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-        
-      });
-  
-      // Return a '200 OK' response to all events
-      res.status(200).send('EVENT_RECEIVED');
-  
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
+            // Get the webhook event. entry.messaging is an array, but 
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+
     } else {
-      // Return a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
 }
 
@@ -56,17 +64,88 @@ let getWebHook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.FB_MSGR_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
 
 
