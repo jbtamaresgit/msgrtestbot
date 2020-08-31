b6de65e4676ebd11240a83e66c10edd0d21246ed
jbtamaresgit <jbtamaresgit@outlook.com>
2020-08-31 20:08:15 +0800

    change env port to PORT index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 initWebRoute(app);
 
-let port = process.env.port || 8080;
+let port = process.env.PORT || 8080;
 
 app.listen(port, () => {console.log(`App is running using ${port}`)});
 
