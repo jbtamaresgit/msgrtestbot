f3a74c66b727ed1f9e6af468cf188d5a9563824d
jbtamaresgit <jbtamaresgit@outlook.com>
2020-08-31 16:02:05 +0800

    update post webhook to get sender psid

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -3,23 +3,28 @@ require("dotenv").config();
 let postWebHook = (req, res) => {
     let body = req.body;
 
-    // Checks this is an event from a page subscription
+    // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+        // Get the webhook event. entry.messaging is an array, but 
+        // will only ever contain one event, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-
-            // Gets the message. entry.messaging is an array, but 
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+        
+      });
+  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+  
     } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
     }
 }
 
@@ -48,6 +53,22 @@ let getWebHook = (req, res) => {
         }
     }
 }
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 
 module.exports = {
     postWebHook: postWebHook,
