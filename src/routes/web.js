6fbaa6c723147bcef27f562c6bf341495b0b237d
jbtamaresgit <jbtamaresgit@outlook.com>
2020-08-31 14:37:39 +0800

    Add chat controller for facebook webhook Update web js for chatcontroller route

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import chatController from "../controllers/chatController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-
+    router.get("/webhook", chatController.getWebHook);
+    router.get("/webhook", chatController.postWebHook);
     // router.get("/", (req, res) => {
     //     return res.send("Hello World");
     // });
